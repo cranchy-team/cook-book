147a212d15021ad68dbc1a5fc81842f39cb6d341
bukabtw <bukarev.k11@gmail.com>
2026-06-05 20:04:31 +0300

    fix(recipe): фикс connect_timeout для sqlite в тестах

--- recipe/app/database.py
+++ recipe/app/database.py
@@ -5,9 +5,14 @@ from .config import get_settings
 
 settings = get_settings()
 
+if "sqlite" in settings.DATABASE_URL:
+    connect_args = {"check_same_thread": False}
+else:
+    connect_args = {"connect_timeout": 10}
+
 engine = create_engine(
     settings.sqlalchemy_database_url,
-    connect_args={"connect_timeout": 10}
+    connect_args=connect_args
 )
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
